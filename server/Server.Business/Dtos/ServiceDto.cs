958d868f8fdc985801e74444b4032c8643048d07
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2024-12-27 08:36:08 +0700

    update response

--- server/Server.Business/Dtos/ServiceDto.cs
+++ server/Server.Business/Dtos/ServiceDto.cs
@@ -13,9 +13,13 @@ namespace Server.Business.Dtos
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Duration { get; set; }
+        
+        public string Status { get; set; }
+        
+        public int CategoryId { get; set; }
+        
         public CategoryDetailDto Category { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime UpdatedDate { get; set; }
-        public string Status { get; set; }
     }
 }
