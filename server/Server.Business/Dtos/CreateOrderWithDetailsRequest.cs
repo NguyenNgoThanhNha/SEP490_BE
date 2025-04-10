672f39a3f3524c54185190d280a5d40dcb6eb6f2
Huy Tran <tranhoanghuy489@gmail.com>
2025-04-10 13:50:05 +0700

    update create-full order

--- server/Server.Business/Dtos/CreateOrderWithDetailsRequest.cs
+++ server/Server.Business/Dtos/CreateOrderWithDetailsRequest.cs
@@ -22,7 +22,7 @@ namespace Server.Business.Dtos
 
         [Range(1, double.MaxValue, ErrorMessage = "Tiền ship phải lớn hơn 0")]
         public decimal ShippingCost { get; set; } 
-        public DateTime EstimatedDeliveryDate { get; set; } 
+        public DateTime? EstimatedDeliveryDate { get; set; } 
         public string? RecipientName { get; set; } 
         public string? RecipientAddress { get; set; } 
         public string? RecipientPhone { get; set; }
