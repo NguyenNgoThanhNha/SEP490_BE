3aba53218007e862e41a5a017f0bfa727161cf1e
Huy Tran <tranhoanghuy489@gmail.com>
2025-04-11 15:50:23 +0700

    API assign service to ServiceRoutine

---
+++ server/Server.Business/Dtos/AssignServiceToRoutineDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Business.Dtos
+{
+    public class AssignServiceToRoutineDto
+    {
+        [Required]
+        public int ServiceId { get; set; }
+
+        [Required]
+        public int RoutineId { get; set; }
+
+      
+    }
+
+}
