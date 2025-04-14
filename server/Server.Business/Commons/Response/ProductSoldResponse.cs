67015e0c0403a32a89b5145377f2ed80b78e8b55
Huy Tran <tranhoanghuy489@gmail.com>
2025-04-14 23:56:54 +0700

    Lượng sản phẩm bán ra của 1 chi nhánh

---
+++ server/Server.Business/Commons/Response/ProductSoldResponse.cs
@@ -0,0 +1,15 @@
+﻿using Server.Business.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Business.Commons.Response
+{
+    public class ProductSoldResponse
+    {
+        public List<ProductSoldByBranchDto> Items { get; set; }
+        public int TotalQuantitySold { get; set; }
+    }
+}
