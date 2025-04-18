9ab346244bfa93905ec04cd79311a69c16e1df81
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2025-04-18 15:51:55 +0700

    feat: GetListShiftWithServiceOfStaff

---
+++ server/Server.Business/Commons/Response/GetListShiftWithServiceOfStaffResponse.cs
@@ -0,0 +1,16 @@
+﻿using Server.Business.Models;
+
+namespace Server.Business.Commons.Response;
+
+public class GetListShiftWithServiceOfStaffResponse
+{
+    public int ServiceId { get; set; }
+    
+    public List<StaffWithMultipleShift> WorkingStaffs { get; set; } = new List<StaffWithMultipleShift>();
+}
+
+public class StaffWithMultipleShift
+{
+    public StaffModel Staff { get; set; }
+    public List<ShiftModel> Shifts { get; set; } = new List<ShiftModel>();
+}
