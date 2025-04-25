5b9c6f0f91d74b39d2de62c0cd004b0d7f7452e4
Huy Tran <tranhoanghuy489@gmail.com>
2025-04-25 23:34:18 +0700

    update appointment booking-statistics

---
+++ server/Server.Business/Dtos/MonthlyStatDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Business.Dtos
+{
+    public class MonthlyStatDto
+    {
+        public int Month { get; set; }
+        public int TotalBookings { get; set; }
+        public int TotalServicesBooked { get; set; }
+    }
+
+}
