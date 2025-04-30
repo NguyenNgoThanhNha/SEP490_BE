bb768b74f5dc594f780b9fea8d226f5f25c442ba
NguyenNha <nguyenngothanhnha2003kg@gmail.com>
2025-04-30 21:46:33 +0700

    fix create notification

--- server/Server.Business/Services/AppointmentsService.cs
+++ server/Server.Business/Services/AppointmentsService.cs
@@ -317,7 +317,7 @@ public class AppointmentsService
                 {
                     var notification = new Notifications()
                     {
-                        CustomerId = customer.UserId,
+                        CustomerId = userId,
                         Content = $"Bạn có cuộc hẹn mới  {staff.StaffInfo.FullName} vào lúc {newAppointment.AppointmentsTime}",
                         Type = "Appointment",
                         isRead = false,
